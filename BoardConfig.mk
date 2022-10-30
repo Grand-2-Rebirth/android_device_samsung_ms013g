88ffb8e4033998481b89eda4190e9f88714b2010
MoazSalem <moaztalaat6@gmail.com>
2022-10-30 15:05:32 +0000

    Initialize repo

---
+++ BoardConfig.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# inherit from common ms01
+include device/samsung/ms01-common/BoardConfigCommon.mk
+
+# Partitions
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A7DEA0
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1866465280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12562643968
+BOARD_CACHEIMAGE_PARTITION_SIZE := 218103808
+TARGET_USERIMAGES_USE_F2FS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := f2fs
+
+# Kernel
+TARGET_KERNEL_CONFIG := lineage_ms013g_defconfig
+
+# Radio/RIL
+include $(COMMON_PATH)/radio/dual/board.mk
+
+# inherit from the proprietary version
+-include vendor/samsung/ms013g/BoardConfigVendor.mk
